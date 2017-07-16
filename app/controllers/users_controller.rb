96de445e31a25592c924d01309b925858a248701
Shuhei Taketani <shuhei.taketani@gmail.com>
2017-07-16 15:16:08 +0900

    Q5回答

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,8 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
 
   def index
-    @users = User.order(:name).paginate(page: params[:page])
+    # @users = User.order(:name).paginate(page: params[:page])
+    @users = User.order(:name)
   end
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161113000344) do
+ActiveRecord::Schema.define(version: 20170716061333) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20161113000344) do
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name"
   end
 
 end
