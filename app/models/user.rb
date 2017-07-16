07908ab5ebb6a9d158f8e382a8ab1b7974abd03f
Shuhei Taketani <shuhei.taketani@gmail.com>
2017-07-16 16:31:41 +0900

    Q8回答

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,6 @@ class User < ApplicationRecord
   has_many :followers, through: :passive_relationships, 
                         source: :follower
 
-  validates :name,  presence: true, 
-                      length: { maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
                       length: { maximum: 255 },
