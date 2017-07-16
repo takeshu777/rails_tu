39d60df3bdc16fcd689a8eee5cbdf9ee38d13b8d
Shuhei Taketani <shuhei.taketani@gmail.com>
2017-07-16 14:17:42 +0900

    ストロングパラメーター削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -68,10 +68,9 @@ class UsersController < ApplicationController
   private
   
     def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+
     end
-    
+
     # 正しいユーザーかどうか確認
     def correct_user
       @user = User.find(params[:id])
